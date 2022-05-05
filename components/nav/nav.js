94fb44b1e170faf0430c9d194f12d2b62f0f7667
Esneider Ospina <sneuder8@gmail.com>
2022-05-05 14:43:06 -0500

    More examples and responsive nav

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,8 +1,8 @@
 const Nav = () => {
   return (
-    <nav className="flex h-16 p-3 bg-rose-300">
+    <nav className="flex h-16 p-3 bg-rose-300 text-stone-800">
       <form className="flex w-full">
-        <input type="text" className="w-1/3 px-5 rounded" placeholder="Search" />
+        <input type="text" className="w-full px-5 rounded md:w-1/3" placeholder="Search" />
         <input type="submit" className="px-5 ml-3 bg-white rounded" value="Search" />
       </form>
     </nav>
--- components/notes/notes.js
+++ components/notes/notes.js
@@ -5,7 +5,20 @@ const Notes = () => {
         <h3 className="font-bold">Comprar dulces de navidad</h3>
         <p className="pb-1 mb-1 text-xs border-b text-stone-500 ">02/05/1956</p>
 
-        <p className="font-extralight">
+        <p className="text-sm font-extralight">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English.{" "}
+        </p>
+      </div>
+
+      <div className="p-4 border rounded">
+        <h3 className="font-bold">Comprar dulces de navidad</h3>
+        <p className="pb-1 mb-1 text-xs border-b text-stone-500 ">02/05/1956</p>
+
+        <p className="text-sm font-extralight">
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
           using Lorem Ipsum is that it has a more-or-less normal distribution of
