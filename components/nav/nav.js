28023a9d06426312ccc0e72dfdff151f91536f50
Esneider Ospina <sneuder8@gmail.com>
2022-05-05 11:49:13 -0500

    Basic styles for navbar

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,7 +1,10 @@
 const Nav = () => {
   return (
-    <nav className="flex h-16 p-3 bg-rose-400">
-      <input type="text" className="px-5 rounded" placeholder="Search" />
+    <nav className="flex h-16 p-3 bg-rose-300">
+      <form className="flex w-full">
+        <input type="text" className="w-1/3 px-5 rounded" placeholder="Search" />
+        <input type="submit" className="px-5 ml-3 bg-white rounded" value="Search" />
+      </form>
     </nav>
   );
 };
