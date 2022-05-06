e6be1373c07cddda4461f1ea908af518d5aaf612
Esneider Ospina <sneuder8@gmail.com>
2022-05-05 21:38:41 -0500

    Responsive nav

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,10 +1,26 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 const Nav = () => {
+  const router = useRouter();
+
   return (
-    <nav className="flex h-16 p-3 bg-rose-300 text-stone-800">
-      <form className="flex w-full">
-        <input type="text" className="w-full px-5 rounded md:w-1/3" placeholder="Search" />
-        <input type="submit" className="px-5 ml-3 bg-white rounded" value="Search" />
+    <nav className="flex h-16 p-3 bg-white shadow-sm text-stone-800">
+      <form className="flex w-full mr-3">
+        <input
+          type="text"
+          className="w-full px-5 rounded md:w-1/3 bg-slate-100"
+          placeholder="Search"
+        />
       </form>
+      <button
+        className="w-auto px-3 text-white rounded bg-slate-800"
+        onClick={() => router.push("/?mode=create")}
+      >
+        <Link href="/?mode=create">
+          <a>Note</a>
+        </Link>
+      </button>
     </nav>
   );
 };
