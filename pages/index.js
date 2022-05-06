c1240c3a44eadb05a26ee58451702e9194d29f5b
Esneider Ospina <sneuder8@gmail.com>
2022-05-05 21:39:18 -0500

    External components

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
-import Notes from '../components/notes/notes'
-import Nav from '../components/nav/nav'
+import Notes from "../components/notes/notes";
+import Nav from "../components/nav/nav";
+import Modal from "../components/modal/modal";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <>
-    <Head>
-      <title>Notes</title>
-    </Head>
-    <Nav />
-    <Notes />
+      <Head>
+        <title>Notes</title>
+      </Head>
+      <Nav />
+      <Notes />
+      {(router.query.note || router.query.mode) && <Modal />}
     </>
-  )
+  );
 }
